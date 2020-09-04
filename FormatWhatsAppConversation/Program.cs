dc3bcba846a0798702746f47d57d78880fc09e93
Duncan Ritchie <duncan.ritchie@informationcatalyst.com>
2020-09-04 16:50:09 +0100

    Encryption message omitted; title formatted; more console-logs

--- FormatWhatsAppConversation/Program.cs
+++ FormatWhatsAppConversation/Program.cs
@@ -51,21 +51,37 @@ namespace FormatWhatsAppConversation
     {
         public static List<string> ConvertToDocumentText(this string[] textLines)
         {
+            Console.WriteLine("Re-arranging the text for the document...");
             List<string> formattedText = new List<string> { };
 
-            Regex regex = new Regex(@"\[(?<datetime>\d\d\/\d\d\/\d\d\d\d, \d\d:\d\d:\d\d)\] (?<author>[^:]+): (?<content>.*$)");
+            Regex mainRegex = new Regex(@"\[(?<datetime>\d\d\/\d\d\/\d\d\d\d, \d\d:\d\d:\d\d)\] (?<author>[^:]+): (?<content>.*$)");
+            Regex titleRegex = new Regex(@"\[(?<datetime>\d\d\/\d\d\/\d\d\d\d, \d\d:\d\d:\d\d)\] (?<creator>.+) created group “(?<title>.+)”");
+            string groupStartMessage = "Messages and calls are end-to-end encrypted. No one outside of this chat, not even WhatsApp, can read or listen to them.";
 
             foreach (string line in textLines)
             {
                 if (line.Trim().Equals("")) { continue; }
 
-                Match match = regex.Match(line);
-                if (match.Success)
+                Match mainMatch = mainRegex.Match(line);
+                if (mainMatch.Success)
                 {
-                    //Console.WriteLine($"{match.Groups["author"]} at {match.Groups["datetime"]} said: {match.Groups["content"]}");
+                    if (mainMatch.Groups["content"].Value.Contains(groupStartMessage))
+                    {
+                        Console.WriteLine("Ignoring WhatsApp’s message about encryption.");
+                    }
+                    else
+                    {
+                        formattedText.Add($"{mainMatch.Groups["author"].Value} — {mainMatch.Groups["datetime"].Value}");
+                        formattedText.Add(mainMatch.Groups["content"].Value);
+                    }
+                    continue;
+                }
 
-                    formattedText.Add($"{match.Groups["author"].Value} — {match.Groups["datetime"].Value}");
-                    formattedText.Add(match.Groups["content"].Value);
+                Match titleMatch = titleRegex.Match(line);
+                if (titleMatch.Success)
+                {
+                    formattedText.Add(titleMatch.Groups["title"].Value);
+                    formattedText.Add($"{titleMatch.Groups["creator"]} created this group at {titleMatch.Groups["datetime"]}");
                 }
                 else
                 {
@@ -73,12 +89,15 @@ namespace FormatWhatsAppConversation
                 }
             }
 
+            Console.WriteLine("The text has been arranged into the format for the document.");
             return formattedText;
         }
 
         public static Document WriteToDocument(this List<string> text, Document document)
         {
+            Console.WriteLine("Writing the text to the document...");
             document.Content.Text = Join(text);
+            Console.WriteLine("The text has been written to the document.");
             return document;
         }
 
@@ -94,6 +113,11 @@ namespace FormatWhatsAppConversation
 
         public static void ApplyHeadingStyle(this Document document)
         {
+            string titleText = document.Paragraphs[1].Range.Text.Trim();
+            document.Paragraphs[1].set_Style(WdBuiltinStyle.wdStyleTitle);
+            Console.WriteLine($"“{titleText}” has been styled as the document title.");
+
+            Console.WriteLine("Applying heading styles...");
             Regex headingRegex = new Regex(@"(?<author>[^—]+) — (?<datetime>\d\d\/\d\d\/\d\d\d\d, \d\d:\d\d:\d\d)$");
             foreach (Paragraph paragraph in document.Paragraphs)
             {
@@ -103,6 +127,7 @@ namespace FormatWhatsAppConversation
                     paragraph.set_Style(WdBuiltinStyle.wdStyleHeading2);
                 }
             }
+            Console.WriteLine("All heading styles have been applied.");
         }
     }
 }
